1be9c876dc9d1513cf494f57020f2b4964b0b636
AdrianNetto <adrian.netto73@gmail.com>
2024-04-30 16:50:07 -0300

    first CRUD in nest

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -11,4 +11,44 @@ export class CoursesService {
       tags: ["NestJS", "NodeJS", "JavaScript", "TypeScript"]
     }
   ]
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: number) {
+    const findCourse = this.courses.find(course => course.id === id);
+
+    if(!findCourse) {
+      return "Course not found :("
+    }
+  }
+
+  create(createCourseDTO: any) {
+    this.courses.push(createCourseDTO);
+  }
+
+  update(id: number, updateCourseDTO: any) {
+    const existingCourse = this.findOne(id);
+    if (existingCourse) {
+      const index = this.courses.findIndex(course => course.id === id);
+      this.courses[index] = {
+        id,
+        ...updateCourseDTO
+      }
+    } else {
+      return "Course not found :("
+    }
+  }
+
+  remove(id: number) {
+    const index = this.courses.findIndex(course => course.id === id);
+
+    if (index >= 0) {
+      this.courses.splice(index, 1)
+      return "Course deleted!"
+    } else {
+      return "Course not found :("
+    }
+  }
 }
