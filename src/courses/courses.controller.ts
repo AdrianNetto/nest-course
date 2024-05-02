b078013de7357450e01266cb042de35edfd01f6d
AdrianNetto <adrian.netto73@gmail.com>
2024-05-02 09:57:13 -0300

    feat: first steps with DTO

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -8,6 +8,8 @@ import {
   Put,
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
+import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
 
 @Controller('courses')
 export class CoursesController {
@@ -24,13 +26,13 @@ export class CoursesController {
   }
 
   @Post()
-  create(@Body() body: any) {
-    return this.courseService.create(body);
+  create(@Body() createCourseDTO: CreateCourseDTO) {
+    return this.courseService.create(createCourseDTO);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() body): string {
-    return this.courseService.update(id, body);
+  update(@Param('id') id: number, @Body() updateCourseDTO: UpdateCourseDTO) {
+    return this.courseService.update(id, updateCourseDTO);
   }
 
   @Delete(':id')
